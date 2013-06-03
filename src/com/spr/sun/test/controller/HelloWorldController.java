eae8d4011c396cf8f5c542d03e33f982681ffaba
sundar <sundara.venkat@gmail.com>
2013-06-03 10:15:36 +0530

    FilesAdded
    First check-in

---
+++ src/com/spr/sun/test/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.spr.sun.test.controller;
+
+public class HelloWorldController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
