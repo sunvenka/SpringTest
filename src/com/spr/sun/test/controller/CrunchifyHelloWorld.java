e77e665c1e50d55da8c5876388d90a392e5e4550
Sundara <sundarrajan.venkatachalam@jda.com>
2013-06-04 15:27:35 +0530

    Actual Libs added.

---
+++ src/com/spr/sun/test/controller/CrunchifyHelloWorld.java
@@ -0,0 +1,18 @@
+package com.spr.sun.test.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+public class CrunchifyHelloWorld {
+ 
+    @RequestMapping("/welcome")
+    public ModelAndView helloWorld() {
+ 
+        String message = "<br><div align='center'>" + "<h1>Hello World, Spring 3.2.1 Example by Crunchify.com<h1> <br>";
+        message += "<a href='http://crunchify.com/category/java-web-development-tutorial/'>More Examples</a>";
+        return new ModelAndView("welcome", "message", message);
+    }
+}
--- src/com/spr/sun/test/controller/HelloWorldController.java
+++
@@ -1,8 +0,0 @@
-package com.spr.sun.test.controller;
-
-
-public class HelloWorldController {
-
-		
-
-}
